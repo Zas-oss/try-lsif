30ea94a24dcc737cbff72af5363d7d02ff5ffff2
z <72273286+Zas-oss@users.noreply.github.com>
2020-12-23 02:01:26 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
-let a = 3;
+//JavaScript program to swap two variables
 
+function prompt() {}
 
-let b = a * 2;
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
